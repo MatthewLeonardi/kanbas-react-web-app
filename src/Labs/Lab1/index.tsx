2a6cdffa3812253a7dda2224cfa2ef53ee4d6b17
Matthew Leonardi <matthewleonardi@Matthews-MacBook-Pro.local>
2024-09-25 01:52:31 -0400

    first commit

---
+++ src/Labs/Lab1/index.tsx
@@ -0,0 +1,154 @@
+export default function Lab1() {
+    return (
+      <div id="wd-lab1">
+        <h2>Lab 1</h2>
+        <h3>HTML Examples</h3>
+        <div id="wd-h-tag">
+          <h4>Heading Tags</h4>
+          ...
+        </div>
+        <div id="wd-p-tag">
+          
+          <h4>Paragraph Tag</h4>
+          <p id="wd-p-1"> ... </p>
+          <p id="wd-p-2">
+  
+  This is the first paragraph. The paragraph tag is used to format
+  vertical gaps between long pieces of text like this one.
+  </p>
+  <p id="wd-p-3">
+  
+  This is the second paragraph. Even though there is a deliberate white
+  gap between the paragraph above and this paragraph, by default
+  browsers render them as one contiguous piece of text as shown here on
+  the right.
+  </p>
+  <p id="wd-p-4">
+  This is the third paragraph. Wrap each paragraph with the paragraph
+  tag to tell browsers to render the gaps.
+  </p>
+  <ol id="wd-pancakes">
+        <h4>List Tags</h4>
+        <h5>Ordered List Tag</h5>
+        How to make pancakes:
+        <li> Mix dry ingredients.</li>
+        <li>Add wet ingredients.</li>
+        <li>Stir to combine.</li>
+        <li>Heat a skillet or griddle.</li>
+        <li>Pour batter onto the skillet.</li>
+        <li>Cook until bubbly on top.</li>
+        <li>Flip and cook the other side.</li>
+        <li>Serve and enjoy!</li>
+        </ol>
+        My favorite recipe:
+<ol id="wd-my-favorite-recipe">{/* complete on your own */}</ol>
+<h5>Unordered List Tag</h5>
+
+My favorite books (in no particular order)
+<ul id="wd-my-books">
+  <li>Dune</li>
+  <li>Lord of the Rings</li>
+  <li>Ender's Game</li>
+  <li>Red Mars</li>
+  <li>The Forever War</li>
+</ul>
+Your favorite books (in no particular order)
+<ul id="wd-your-books">
+  {/* complete on your own */}
+</ul>
+        </div>
+        <div id="wd-tables">
+        <h4>Table Tag</h4>
+        <table border={1} width="100%">
+          <thead>
+            <tr>
+              <th>Quiz</th>
+              <th>Topic</th>
+              <th>Date</th>
+              <th>Grade</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Q1</td>
+              <td>HTML</td>
+              <td>2/3/21</td>
+              <td>85</td>
+            </tr>
+            <tr>
+              <td>Q2</td>
+              <td>CSS</td>
+              <td>2/10/21</td>
+              <td>90</td>
+            </tr>
+            <tr>
+              <td>Q3</td>
+              <td>JavaScript</td>
+              <td>2/17/21</td>
+              <td>95</td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan={3}>Average</td>
+              <td>90</td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+      <h5 id="wd-radio-buttons">Radio buttons</h5>
+
+<label>Favorite movie genre:</label><br />
+
+<input type="radio" name="radio-genre" id="wd-radio-comedy"/>
+<label htmlFor="wd-radio-comedy">Comedy</label><br />
+
+<input type="radio" name="radio-genre" id="wd-radio-drama"/>
+<label htmlFor="wd-radio-drama">Drama</label><br />
+
+<input type="radio" name="radio-genre" id="wd-radio-scifi"/>
+<label htmlFor="wd-radio-scifi">Science Fiction</label><br />
+
+<input type="radio" name="radio-genre" id="wd-radio-fantasy"/>
+<label htmlFor="wd-radio-fantasy">Fantasy</label>
+
+<h5 id="wd-checkboxes">Checkboxes</h5>
+<label>Favorite movie genre:</label><br/>
+
+<input type="checkbox" name="check-genre" id="wd-chkbox-comedy"/>
+<label htmlFor="wd-chkbox-comedy">Comedy</label><br/>
+
+<input type="checkbox" name="check-genre" id="wd-chkbox-drama"/>
+<label htmlFor="wd-chkbox-drama">Drama</label><br/>
+
+<input type="checkbox" name="check-genre" id="wd-chkbox-scifi"/>
+<label htmlFor="wd-chkbox-scifi">Science Fiction</label><br/>
+
+<input type="checkbox" name="check-genre" id="wd-chkbox-fantasy"/>
+<label htmlFor="wd-chkbox-fantasy">Fantasy</label>
+
+<h4 id="wd-dropdowns">Dropdowns</h4>
+
+<h5>Select one</h5>
+<label htmlFor="wd-select-one-genre"> Favorite movie genre: </label><br/>
+<select id="wd-select-one-genre">
+   <option value="COMEDY">Comedy</option>
+   <option value="DRAMA">Drama</option>
+   <option selected value="SCIFI">
+       Science Fiction</option>
+   <option value="FANTASY">Fantasy</option>
+</select>
+
+<h5>Select many</h5>
+<label htmlFor="wd-select-many-genre"> Favorite movie genres: </label><br/>
+<select id="wd-select-many-genre" multiple>
+   <option selected value="COMEDY">Comedy</option>
+   <option value="DRAMA">Drama</option>
+   <option selected value="SCIFI">
+       Science Fiction</option>
+   <option value="FANTASY">Fantasy</option>
+</select>
+      </div>
+    );
+  }
+  
