2e1685243e96eb8aad7c20e66762c65b952f63d2
Matthew Leonardi <matthewleonardi@Matthews-MacBook-Pro.local>
2024-10-10 21:21:06 -0400

    A2 Push

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -3,23 +3,19 @@ import Account from "./Account";
 import Dashboard from "./Dashboard";
 import KanbasNavigation from "./Navigation";
 import Courses from "./Courses";
+import "./styles.css";
 export default function Kanbas() {
   return (
     <div id="wd-kanbas">
-      <table>
-        <tr>
-          <td valign="top">
-            <KanbasNavigation />
-          </td>
-          <td valign="top">
+      <KanbasNavigation />
+      <div className="wd-main-content-offset p-3">
       <Routes>
         <Route path="/" element={<Navigate to="Account" />} />
         <Route path="/Account/*" element={<Account />} />
         <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/Courses/:cid/*" element={<Courses />} />
       </Routes>
-      </td>
-      </tr>
-      </table>
     </div>
+   </div>
   );
 }
